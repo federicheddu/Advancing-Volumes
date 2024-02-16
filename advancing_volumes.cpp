90db69b4653666bba0db75156889890c593b8f3a
Federico Meloni <39567979+federicheddu@users.noreply.github.com>
2024-02-16 12:44:12 +0100

    expand

--- advancing_volumes.cpp
+++ advancing_volumes.cpp
@@ -6,8 +6,8 @@ void advancing_volume(Data &d) {
     cout << BMAG << "Advancing Volume ITERATION " << d.step << RESET << endl;
 
     if(d.running) expand(d);
-    if(d.running) refine(d);
-    if(d.running) smooth(d);
+    //if(d.running) refine(d);
+    //if(d.running) smooth(d);
 
     cout << BMAG << "Advancing Volume ITERATION " << BGRN << "DONE" << RESET << endl;
 
@@ -15,26 +15,84 @@ void advancing_volume(Data &d) {
 
 void expand(Data &d) {
 
+    if(d.verbose) cout << BCYN << "Expanding model..." << RESET;
+    if(d.render) d.gui->pop_all_markers();
+
+    //compute DDD: direction, distance and displacement -> saved into .vert_data(vid).normal
+    //compute_directions(d);
+    compute_distances(d);
+    compute_displacements(d);
+
+    //priority queue for the front
+    auto cmp = [&](uint a, uint b) { return d.m.vert_data(a).uvw[DIST] > d.m.vert_data(b).uvw[DIST]; };
+    std::sort(d.front.begin(), d.front.end(), cmp);
+
+    //move the verts
+    for(uint vid : d.front) {
+        move(d, vid, d.m.vert_data(vid).normal);
+        if(!d.running) return;
+    }
+
+    //restore normals, update fronts, check for self intersections
+    d.m.update_normals();
+    /* TODO: update fronts here */
+    check_self_intersection(d);
+    if(!d.running) return;
+
+    if(d.verbose) cout << BGRN << "DONE" << RESET << endl;
+}
+
+void move(Data &d, uint vid, vec3d &disp) {
+    CGAL_Q rt_disp[3];
+    to_rational(disp, rt_disp);
+    move(d, vid, rt_disp);
+}
+
+void move(Data &d, uint vid, CGAL_Q *rt_disp) {
+
+    bool moved = true;
+
+    //backup
+    CGAL_Q rt_safe_pos[3];
+    copy(&d.rationals[3*vid], rt_safe_pos);
+
+    //new position of the vert
+    CGAL_Q rt_moved[3] = {d.rationals[3*vid+0] + rt_disp[0],
+                          d.rationals[3*vid+1] + rt_disp[1],
+                          d.rationals[3*vid+2] + rt_disp[2]};
+
+    /* TODO: topological unlock */
+
+    //update the vert
+    d.m.vert(vid) = to_double(rt_moved);
+    copy(rt_moved, &d.rationals[3*vid]);
+
+    line_search(d, vid, rt_safe_pos);
+    if(!d.running) return;
+
+    /* TODO: snap rounding */
+
+    //update the active flag
+    if(dist_calc(d, vid, true) < d.inactivity_dist) d.m.vert_data(vid).flags[ACTIVE] = false;
+
 }
 
 void compute_directions(Data &d) {
 
-    vec3d move;
-    std::vector<uint> adjs;
+    vec3d direction;
 
     //direction smoothing
     for(int it = 0; it < d.smooth_dir_iters; it++) {
         for(uint vid : d.front) {
             //default movement
-            move = d.m.vert_data(vid).normal;
+            direction = d.m.vert_data(vid).normal;
             //avg
-            adjs = d.m.vert_adj_srf_verts(vid);
-            for(uint adj : adjs)
-                move += d.m.vert_data(adj).normal;
-            move /= double(adjs.size()+1);
-            move.normalize();
+            for(uint adj : d.m.vert_adj_srf_verts(vid))
+                direction += d.m.vert_data(adj).normal;
+            direction /= (d.m.vert_adj_srf_verts(vid).size() + 1);
+            direction.normalize();
             //update after smoothing
-            d.m.vert_data(vid).normal = move;
+            d.m.vert_data(vid).normal = direction;
         }
     }
 
@@ -45,16 +103,18 @@ void compute_distances(Data &d) {
     double dist;
     for(uint vid : d.front) {
         dist = dist_calc(d, vid, d.only_raycast);
-        if(!d.only_raycast && dist < d.inactivity_dist < d.switch_raycast_threshold)
+        if(!d.only_raycast && dist < d.switch_raycast_threshold)
             dist = dist_calc(d, vid, true);
         d.m.vert_data(vid).uvw[DIST] = dist;
     }
 
 }
 
-void compute_displacement(Data &d) {
+void compute_displacements(Data &d) {
+
     for(uint vid : d.front)
-        d.m.vert_data(vid).normal = d.m.vert_data(vid).normal * d.m.vert_data(vid).uvw[DIST] * 0.99;
+        d.m.vert_data(vid).normal *= d.m.vert_data(vid).uvw[DIST] * 0.99;
+
 }
 
 void line_search(Data &d, uint vid, CGAL_Q *safe_pos) {
@@ -101,16 +161,34 @@ void line_search(Data &d, uint vid, CGAL_Q *safe_pos) {
 
 //check if the vert umbrella is intersecting the target
 bool check_intersection(Data &d, uint vid) {
+
     //params
-    std::unordered_set<uint> intersect_ids;
     bool flag = false;
+    vec3d verts[3];
+    std::unordered_set<uint> intersect_ids; //warning killer
 
     //check the umbrella
     for (uint fid : d.m.vert_adj_srf_faces(vid)) {
-        vec3d verts[] = {d.m.face_verts(fid)[0], d.m.face_verts(fid)[1], d.m.face_verts(fid)[2]};
+        verts[0] = d.m.face_verts(fid)[0];
+        verts[1] = d.m.face_verts(fid)[1];
+        verts[2] = d.m.face_verts(fid)[2];
         flag = d.oct->intersects_triangle(verts, false, intersect_ids);
         if(flag) break; //early stop
     }
 
     return flag;
+}
+
+void check_self_intersection(Data &d) {
+
+    if(d.verbose) cout << BCYN << "Checking self intersection..." << RESET;
+    std::set<ipair> intersections;
+
+    export_surface(d.m, d.ms);
+    find_intersections(d.ms, intersections);
+    d.running = intersections.empty();
+
+    //feedback
+    if(!d.running) cout << BRED << "FOUND" << RESET << endl;
+
 }
--- advancing_volumes.h
+++ advancing_volumes.h
@@ -2,6 +2,7 @@
 #define ADVANCING_VOLUMES_ADVANCING_VOLUMES_H
 
 #include "advancing_io.h"
+#include <cinolib/find_intersections.h>
 
 void advancing_volume(Data &d);
 
@@ -10,12 +11,14 @@ void expand(Data &d);
 void refine(Data &d);
 void smooth(Data &d);
 
-//expand funcitons
-void compute_direction(Data &d);
+//expand functions
+void move(Data &d, uint vid, vec3d &disp);
+void move(Data &d, uint vid, CGAL_Q *rt_disp);
+void compute_directions(Data &d);
 void compute_distances(Data &d);
-void compute_displacement(Data &d);
+void compute_displacements(Data &d);
 void line_search(Data &d, uint vid, CGAL_Q *safe_pos);
 bool check_intersection(Data &d, uint vid);
-bool check_self_intersection(Data &d);
+void check_self_intersection(Data &d);
 
 #endif //ADVANCING_VOLUMES_ADVANCING_VOLUMES_H
